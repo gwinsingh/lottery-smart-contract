04c9a2668514e465abb644e8cfc9237040550780
Gurwinder Singh <gurwinderiitr@gmail.com>
2018-02-15 16:54:25 +0530

    Add Project Files

---
+++ compile.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "Lottery.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+// module.exports = solc.compile(source, 1).contracts[":Lottery"];
+// Finally Compile the source code.
+console.log(
+  "Compiled Contract: ",
+  JSON.parse(solc.compile(JSON.stringify(input))).contracts["Lottery.sol"]
+    .Lottery.abi
+);
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "Lottery.sol"
+].Lottery;
